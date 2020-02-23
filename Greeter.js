2bef9f1e619a4debf1ea4e9ad45a6f2f10aad97d
Tony Liang <jliang1120tony@gmail.com>
2020-02-23 15:14:18 -0800

    "adding support for jQuery, and use this new library in app.js"

--- Greeter.js
+++ Greeter.js
@@ -1,37 +1,49 @@
-(function(global, $) {
+;(function(global, $) {
 
+    // 'new' an object
     var Greetr = function(firstName, lastName, language) {
         return new Greetr.init(firstName,lastName,language);
     }
+
+    // hidden within the scope of the IIFE and never directly accessible
     var supportedLangs = ['en','es']; // thanks to closures
     
+    // informal greetings
     var greetings = {
         en: 'Hello',
         es: 'Hola'
     };
 
+    // formal greetings
     var formalGreetings = {
         en: 'Greetings',
         es: 'Saludos'
     };
 
+    // logger messages
     var logMessages = {
         en: 'Logged in',
         es: 'Inicio sesion'
     };
 
+    //prototype holds methods (to save memory space)
     Greetr.prototype = {
+
+        // 'this' refers to the calling object at execution time
         fullName: function(){
             return this.firstName+' '+this.lastName;
         },
 
         validate: function() {
+            // check that is a valid language
+            // references the externally inaccessible 'supportedLangs' within the closure
             if(supportedLangs.indexOf(this.language) === -1 ){
 
                 throw "Invalid languae"
             }
         },
 
+        // retrieve messages from object by referring to properties using [] syntax
         greeting: function() {
             return greetings[this.language] +' '+this.firstName + '!';
         },
@@ -40,8 +52,11 @@
             return formalGreetings[this.language] +', ' + this.fullName();
         },
 
+        //chainable methods return their own containing object
         greet: function(formal) {
             var msg;
+
+            // if undefined or null it will be coerced to 'false'
             if(formal) {
                 msg = this.formalGreeting();
             } 
@@ -51,7 +66,9 @@
             if (console) {
                 console.log(msg);
             }
-            return this; // making the method chainable
+            // this refers to the calling object at execution time
+            // makes the method chainable
+            return this; 
         },
 
         log: function() {
@@ -62,10 +79,34 @@
         },
 
         setLang: function(lang) {
+    
             this.language = lang;
             this.validate();
             return this;
+        },
+
+        HTMLGreeting: function(selector, formal) {
+            if(!$) {
+                throw 'jQuery not loaded';
+            }
+            if(!selector) {
+                throw ' Missing jQuery selector';
+            }
+
+            // determin the message
+            var msg;
+            if (formal) {
+                msg = this.formalGreeting();
+            } 
+            else {
+                msg = this.greeting();
+            }
+            $(selector).html(msg);
+            return this;
+
         }
+
+
 
     };
 
@@ -74,6 +115,8 @@
         self.firstName = firstName || '';
         self.lastName = lastName || '';
         self.language = language || 'en';
+
+        self.validate();
     }
 
     Greetr.init.prototype = Greetr.prototype;
--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var g = G$('Tony','Liang');
-console.log(g);
-g.greet().greet(true);
+// console.log(g);
+g.greet().setLang('es').greet(true);
+
+$('#login').click(function(){
+    var loginGrtr = G$('Tony','Liang');
+    $('#logindiv').hide();
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting',true).log();
+});
